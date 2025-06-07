979f52fa7b4febdbaec75b1d2a4baefab724b0de
prateek3189 <prateek.magarde@gmail.com>
2025-06-08 01:02:39 +0530

    Start

---
+++ 01-Getting-Started/gs-01-starting-project/app-classic.js
@@ -0,0 +1,16 @@
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
+
+// function addGoal(e) {
+//   const enteredValue = inputEl.value;
+//   if (enteredValue === "") {
+//     return;
+//   }
+//   const listItem = document.createElement("li");
+//   listItem.textContent = enteredValue;
+//   listEl.appendChild(listItem);
+//   inputEl.value = "";
+// }
+
+// buttonEl.addEventListener("click", addGoal);
