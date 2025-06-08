ea73b5a4bafd163e5adb9c1bdf8a917a365d3769
prateek3189 <prateek.magarde@gmail.com>
2025-06-08 13:26:54 +0530

    Getting Started

---
+++ 01-Getting-Started/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+});
+
+app.mount("#app");
